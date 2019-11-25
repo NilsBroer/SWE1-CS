31887fb46df5ee72095d8119832e4a8136ef2e85
Nils B <44638127+NilsBroer@users.noreply.github.com>
2019-11-25 14:38:28 +0100

    85/106, PluginManager enhanced but unfinished. (And still terrrible)

--- MyWebServer/PluginManager.cs
+++ MyWebServer/PluginManager.cs
@@ -2,27 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using BIF.SWE1.Interfaces;
 
 namespace MyWebServer
 {
     class PluginManager : IPluginManager
     {
-        public IEnumerable<IPlugin> Plugins => throw new NotImplementedException();
+        List<IPlugin> Plugins_in = new List<IPlugin>();
+        public PluginManager()
+        {
+            /*//Delete me, just for pathing reference, if needed later
+            string defplugpath = System.AppDomain.CurrentDomain.BaseDirectory;
+            if (defplugpath.Contains("deploy"))
+                defplugpath += "/../MyWebServer/Plugins";
+            else
+                defplugpath += "/../../SWE1-CS/MyWebServer/Plugins";
+
+            foreach(var plugin in Directory.GetFiles(defplugpath, "*Plugin.cs"))
+            {
+                IPlugin pluginObj = (IPlugin)Activator.CreateInstance(Type.GetType(plugin));
+
+                if (pluginObj == null)
+                {
+                    throw new Exception();
+                }
+                else
+                    Plugins_in.Add(pluginObj);
+            }
+            */
+            
+        }
+        public IEnumerable<IPlugin> Plugins => Plugins_in;
 
         public void Add(IPlugin plugin)
         {
-            throw new NotImplementedException();
+            Plugins_in.Add(plugin);
         }
 
         public void Add(string plugin)
         {
-            throw new NotImplementedException();
+            IPlugin pluginObj = (IPlugin)Activator.CreateInstance(Type.GetType(plugin)); //Examine what this does in more detail
+
+            if (pluginObj == null)
+            {
+                throw new Exception();
+            }
+            else
+                Plugins_in.Add(pluginObj);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Plugins_in.Clear();
         }
     }
 }
