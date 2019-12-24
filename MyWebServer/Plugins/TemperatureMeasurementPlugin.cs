db643c973d42d8b9e598dc4f4b6e940b744b1b48
Nils B <44638127+NilsBroer@users.noreply.github.com>
2019-12-24 15:38:11 +0100

    Temperature Plugin should be finished, except for rest-xml (test-cases solved)

--- MyWebServer/Plugins/TemperatureMeasurementPlugin.cs
+++ MyWebServer/Plugins/TemperatureMeasurementPlugin.cs
@@ -26,7 +26,7 @@ namespace MyWebServer.Plugins
 
         public string GetUrl (DateTime from, DateTime until)
         {
-            return $"/temperature?type=regular&from='${from.ToString("dd/MM/yyyy HH:mm:ss")}'&until='{until.ToString("dd/MM/yyyy HH:mm:ss")}'";
+            return $"/temperature?type=regular&from={from.ToString("dd/MM/yyyy HH:mm:ss")}&until={until.ToString("dd/MM/yyyy HH:mm:ss")}";
         }
 
         public string GetUrl(DateTime day) //Not necessary, template for handling single-day url from indication
@@ -38,41 +38,87 @@ namespace MyWebServer.Plugins
 
         public string GetRestUrl(DateTime from, DateTime until)
         {
-            return $"/temperature?type=rest&from='${from.ToString("dd/MM/yyyy HH:mm:ss")}'&until='{until.ToString("dd/MM/yyyy HH:mm:ss")}'";
+            return $"/temperature?type=rest&from={from.ToString("dd/MM/yyyy HH:mm:ss")}&until={until.ToString("dd/MM/yyyy HH:mm:ss")}";
         }
 
         public IResponse Handle(IRequest req)
         {
             Response response = new Response();
-            if (this.CanHandle(req) > 0.1f)
+            string reqtype = "";
+            req.Url.Parameter.TryGetValue("type", out reqtype);
+            bool is_rest = (reqtype != null && (reqtype.Contains("rest") || reqtype.Contains("restles") || req.Url.Segments.Contains("GetTemperature"))); // == instead of contains also possible, GetTemperature is apparently rest, by indication
+
+            if (this.CanHandle(req) > 0.1f && !is_rest)
             {
                 SqlConnection myConnection = new SqlConnection();
-                //Change so it also works for Chris
+                //TODO: Change so it also works for Chris
                 myConnection.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"C:\\Users\\Nsync\\Google Drive\\UNI\\Semester 03\\SWE_CS\\SWE1-CS\\MyWebServer\\Database\\Database.mdf\"; Integrated Security = True;"; //Look-up in Server Explorer
 
                 Console.WriteLine("Connection created, path: " + myConnection.ConnectionString);
                 myConnection.Open();
                 Console.WriteLine("Connected...");
 
-                //TODO: Get actual values from request-url
-                DateTime until = DateTime.Now;
-                DateTime from = DateTime.Now.AddDays(-500);
+                string fromString, untilString;
+                DateTime from, until;
+                bool from_is_set = false;
+
+                if (from_is_set = req.Url.Parameter.TryGetValue("from", out fromString))
+                    from = Convert.ToDateTime(fromString); //can fail, try-catch?
+                else
+                    from = DateTime.Now.AddDays(-365*10); //max from (roughly) //restrict to segment /all ?
+                if (req.Url.Parameter.TryGetValue("until", out untilString))
+                    until = Convert.ToDateTime(untilString); //can fail, try-catch?
+                else
+                    until = DateTime.Now; //max until
 
                 SqlCommand myCommand = new SqlCommand(null, myConnection);
                 
-                //add if-clauses, different commands for different urls (e.g. single day)
-                //add content-restriction, restrict amount shown per site, add buttons
                 myCommand.CommandText = $"SELECT [Time-Stamp], Value FROM Temperature WHERE [Time-Stamp] between @from and @until;";
                 myCommand.Parameters.AddWithValue("@from", from.ToString()); //check
                 myCommand.Parameters.AddWithValue("@until", until.ToString()); //check
 
+                //Create base for flippable HTML-table
+                string HTML_begin = @"
+                        <!DOCTYPE html>   
+                        <html lang=""en"">   
+                        <head>   
+	                        <meta charset=""utf-8"">   
+	                        <title>Temperatures</title>   
+	                        <meta name=""description"" content=""Bootstrap."">  
+	                        <link href=""http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"" rel=""stylesheet"">   
+	                        <script src=""http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js""></script>
+	                        <link rel=""stylesheet"" href=""http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css""></style>
+	                        <script type=""text/javascript"" src=""http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js""></script>
+	                        <script type=""text/javascript"" src=""http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js""></script>
+                        </head>  
+                        <body style=""margin:20px auto"">  
+	                        <div class=""container"">
+		                        <div class=""row header"" style=""text-align:center;color:green"">
+			                        <h3>Measured Temperatures</h3>
+		                        </div>
+	                        <table id=""myTable"" class=""table table-striped"">
+                    ";
+                string HTML_end = @"
+                        	</table>  
+	                        </div>
+                        </body>  
+
+                        <script>
+                        	$(document).ready(function(){ $('#myTable').dataTable(); });
+                        </script>
+                        </html>
+                    ";
+
+                StringBuilder content = new StringBuilder();
                 SqlDataReader reader = myCommand.ExecuteReader();
+                content.Append(HTML_begin);
+                content.Append("<thead><tr><th>Time-Stampe</th><th>Temperature</th></tr></thead> <tbody>");
                 try
                 {
                     while (reader.Read())
                     {
-                        Console.WriteLine(String.Format("{0}, {1}",
-                        reader["Time-Stamp"], reader["Value"])); //Works for testing :) but Replace with set content
+                        content.Append(String.Format("<tr><td>{0}</td><td>{1}</td></tr>",
+                        reader["Time-Stamp"], reader["Value"]));
                     }
                 }
                 finally
@@ -80,9 +126,24 @@ namespace MyWebServer.Plugins
                     // Always call Close when done reading.
                     reader.Close();
                 }
+                content.Append(HTML_end);
+                content.Append("</tbody> <tfoot><tr><th>Time-Stampe</th><th>Temperature</th></tr></tfoot>");
 
+                response.SetContent(content.ToString());
+                response.ContentType = "text/html";
+                //response.AddHeader();
 
                 //if everything is ok (no internal errors)
+                response.StatusCode = 200;
+            }
+            else if(this.CanHandle(req) > 0.1f && is_rest)
+            {
+                //TODO: implement rest-handle
+                //TODO: implement rest-handle for single day query from indication
+                response.SetContent("REST-XML goes here");
+                response.ContentType = "text/xml";
+                //response.AddHeader();
+
                 response.StatusCode = 200;
             }
             else
