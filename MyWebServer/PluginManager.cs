9b0a4ad75a62dec31733ee79d822d07f38d297c2
Nils B <44638127+NilsBroer@users.noreply.github.com>
2019-12-31 00:35:51 +0100

    All unit-tests running, mind hard-coded paths and comments, some things could be overhault

--- MyWebServer/PluginManager.cs
+++ MyWebServer/PluginManager.cs
@@ -3,66 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Reflection;
-using System.Security.Permissions;
-using MyWebServer.Plugins;
 using BIF.SWE1.Interfaces;
 
 namespace MyWebServer
 {
-
     class PluginManager : IPluginManager
     {
-        private List<IPlugin> Plugins_in = new List<IPlugin>();
+        List<IPlugin> Plugins_in = new List<IPlugin>();
 
         public PluginManager()
         {
-            string myPath = Path.Combine(new string[] { Path.GetDirectoryName(this.GetType().Assembly.Location), "Plugin" });   //aktuelles Directory (deploy) (Hier musst du den Ordner "Plugin" erstellen Nils, einmal im Deploy Ordner einmal bei den unit tests, weist eh
-            string[] dllFileNames = null;
-            if (Directory.Exists(myPath))
-            {
-                dllFileNames = Directory.GetFiles(myPath, "*.dll"); // Alle DLL files finden
-                
-                //Assemblies für dynamisches Laden von Programmteilen (reflections)
-                ICollection<Assembly> assemblies = new List<Assembly>(dllFileNames.Length);
-                foreach (string dllFile in dllFileNames)
-                {
-                    AssemblyName myAssemblyName = AssemblyName.GetAssemblyName(dllFile);
-                    Assembly myAssembly = Assembly.Load(myAssemblyName);
-                    assemblies.Add(myAssembly);
-                }
+            MyWebServer.Plugins.TestPlugin test = new Plugins.TestPlugin();
+            MyWebServer.Plugins.NavigationPlugin navi = new Plugins.NavigationPlugin();
+            MyWebServer.Plugins.StaticFilePlugin stat = new Plugins.StaticFilePlugin();
+            MyWebServer.Plugins.TemperatureMeasurementPlugin temp = new Plugins.TemperatureMeasurementPlugin();
+            MyWebServer.Plugins.ToLowerPlugin tlwr = new Plugins.ToLowerPlugin();
 
-                //den Assemblies verschiedene Plugin-Typen hinzufügen
-                Type pluginType = typeof(IPlugin);
-                ICollection<Type> pluginTypes = new List<Type>();
-                foreach (Assembly assembly in assemblies)
-                {
-                    if (assembly != null)
-                    {
-                        Type[] types = assembly.GetTypes();
-                        foreach (Type type in types)
-                        {
-                            if (type.IsInterface || type.IsAbstract)    //Interfaces ignorieren und Abstracts ignorieren, brauchen ja nur diese Klassen
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                if (type.GetInterface(pluginType.FullName) != null)     //wenn es kein Interface oder Abtract ist, muss es eine Klasse sein, Klasse is gud
-                                {
-                                    pluginTypes.Add(type);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                foreach (Type type in pluginTypes)
-                {
-                    IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
-                    Add(plugin);    //Plugins hinzufügen
-                }
-            }
+            Add(test);
+            Add(navi);
+            Add(stat);
+            Add(temp);
+            Add(tlwr);
         }
 
         public IEnumerable<IPlugin> Plugins => Plugins_in;
@@ -116,37 +77,5 @@ namespace MyWebServer
         {
             Plugins_in.Clear();
         }
-
-        /*public IPlugin GetSpecificPlugin(string pluginP)
-        {
-            Assembly pluginAssembly = LoadPlugin(pluginP);
-            return CreatePlugins(pluginAssembly).First();
-        }*/
-
-        private static IEnumerable<IPlugin> CreatePlugins(Assembly assembly)
-        {
-            foreach (Type type in assembly.GetTypes())
-            {
-                if (typeof(IPlugin).IsAssignableFrom(type))
-                {
-                    if (Activator.CreateInstance(type) is IPlugin result)
-                    {
-                        yield return result;
-                    }
-                }
-            }
-        }
-
-        /*private static Assembly LoadPlugin(string relativePath)
-        {
-            PluginLoadContext loadContext = new PluginLoadContext(relativePath);
-            if (File.Exists(relativePath))
-            {
-                return loadContext.LoadFromAssemblyName(
-                    new AssemblyName(Path.GetFileNameWithoutExtension(relativePath)));
-            }
-
-            return null;
-        }*/
     }
 }
--- MyWebServer/Uebungen/UEB7_custom.cs
+++ MyWebServer/Uebungen/UEB7_custom.cs
@@ -19,5 +19,30 @@ namespace Uebungen
         {
             return new Url(path);
         }
+
+        public IPluginManager GetPluginManager()
+        {
+            return new PluginManager();
+        }
+
+        public IRequest GetRequest(System.IO.Stream network)
+        {
+            return new Request(network);
+        }
+
+        public IPlugin GetTemperaturePlugin()
+        {
+            return new TemperatureMeasurementPlugin();
+        }
+
+        public string GetNaviUrl()
+        {
+            return "/navigation";
+        }
+
+        public IPlugin GetNavigationPlugin()
+        {
+            return new NavigationPlugin();
+        }
     }
 }
